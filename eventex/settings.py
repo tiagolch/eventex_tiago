b5ac4889a3dca31e389894a8af63d488aa47b392
m2b-tiagochaves <tiago.chaves@mobi2buy.com>
2022-08-20 20:10:10 +0100

    updated set

--- eventex/settings.py
+++ eventex/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + BASE_DIR / 'db.sqlite3'
+default_dburl = 'sqlite:///' + str(BASE_DIR)+'db.sqlite3'
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
